627ce76840e3305c2e8434f1a8c0529d7182400a
0UTC4STD <dylancourte@yahoo.com>
2023-05-02 15:08:22 -0400

    Navbar Color

--- src/routes/Navbar.js
+++ src/routes/Navbar.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Logout from './Logout';
 
-const Navbar = ({ handleLogout }) => { 
-     return (
-    <nav>
-      <ul style={{ display: 'flex', justifyContent: 'space-around', listStyleType: 'none' }}>
+const Navbar = ({ handleLogout }) => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-title">Courtemanche Financial</div>
+      <ul className="nav-links">
         <li><Link to="/portfolio">Portfolio</Link></li>
         <li><Link to="/research">Research</Link></li>
         <li><Link to="/buy">Buy</Link></li>
         <li><Link to="/sell">Sell</Link></li>
         <li><Link to="/about">About</Link></li>
-        <li><Logout handleLogout={handleLogout}/></li>
+        <li><Logout handleLogout={handleLogout} /></li>
       </ul>
     </nav>
   );
