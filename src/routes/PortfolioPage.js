4dcb7ca57075c25a7574922a550afc2867517689
0UTC4STD <dylancourte@yahoo.com>
2023-05-02 17:02:24 -0400

    Portfolio Styles

--- src/routes/PortfolioPage.js
+++ src/routes/PortfolioPage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import '../styles/PortfolioPage.css';
 
 const PortfolioPage = ({ stocks }) => {
   const [stocksWithMarketValue, setStocksWithMarketValue] = useState([]);
