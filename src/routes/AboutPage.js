dedeb8721480c29d851360a54af35b63e936dc3c
0UTC4STD <dylancourte@yahoo.com>
2023-05-05 18:00:47 -0400

    About style

--- src/routes/AboutPage.js
+++ src/routes/AboutPage.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const AboutPage = () => {
   return (
-    <div>
+    <div className="aboutpage-container">
+      <div className="rounded-edge-box">  
       <h1>About</h1>
       <div className="about-section">
         <h4 className="about-paragraph">Welcome to our Stock Portfolio Manager WebApp, designed and developed by Dylan Courtemanche in April 2023 as a Capstone Project for his Software Engineer Certification from the University of South Florida. Our WebApp is built in React and uses MongoDB and Heroku to offer a seamless and secure user experience.</h4>
@@ -11,6 +12,7 @@ const AboutPage = () => {
         <h4 className="about-paragraph">At our Stock Portfolio Manager WebApp, we are committed to providing our users with the tools and resources they need to make informed investment decisions. Whether you're a seasoned investor or just getting started, our WebApp is the perfect solution for managing your stock portfolio. Thank you for choosing our Stock Portfolio Manager WebApp, and we look forward to helping you achieve your investment goals.</h4>
       </div>
     </div>
+  </div>
   );
 };
 
